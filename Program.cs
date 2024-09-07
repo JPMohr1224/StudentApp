bc58b2717585bd1ec8e41ac630775cc9f2a5090d
JP Mohr <James.Mohr2.0@hotmail.com>
2024-09-07 14:39:06 -0400

    added addgrades method to class Student

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace StudentApp
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<double> Grades { get; set; }
+
+        // Constructor to initialize the list
+        public Student()
+        {
+            Grades = new List<double>();
+        }
+
+        // Method to add a single grade
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        // Method to add multiple grades using params
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage of Student class
+            Student student = new Student
+            {
+                Name = "John Doe",
+                ID = 12345
+            };
+
+            // Adding a single grade
+            student.AddGrade(85.5);
+
+            // Adding multiple grades
+            student.AddGrade(90.2, 78.3, 88.9);
+
+            Console.WriteLine($"Student: {student.Name}, ID: {student.ID}");
+            Console.WriteLine("Grades: " + string.Join(", ", student.Grades));
+        }
+    }
+}
